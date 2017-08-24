e5d568b07128c2b6219b943300deb7d7347fb58f
wuwenbin1 <wuwenbin1@jd.com>
2017-08-24 19:00:04 +0800

    chapter5

---
+++ chapter5/downloadAll.py
@@ -0,0 +1,44 @@
+import os
+from urllib.request import urlretrieve
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+downloadDirectory = "downloaded"
+baseUrl = "http://pythonscraping.com"
+
+
+# 获得绝对路径
+def getAbsoluteURL(baseUrl, source):
+    if source.startswith("http://www."):
+        url = "http://" + source[11:]
+    elif source.startswith("http://"):
+        url = source
+    elif source.startswith("www."):
+        url = "http://" + source[4:]
+    else:
+        url = baseUrl + "/" + source
+    if baseUrl not in url:
+        return None
+    return url
+
+
+def getDownloadPath(baseUrl, absoluteUrl, downloadDirectory):
+    path = absoluteUrl.replace("www.", "")
+    path = path.replace(baseUrl, "")
+    path = downloadDirectory + path
+    directory = os.path.dirname(path)
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    return path
+
+
+html = urlopen("http://www.pythonscraping.com")
+bsObj = BeautifulSoup(html, "html.parser")
+downloadList = bsObj.findAll(src=True)  # 选择所有带src的标签
+
+for download in downloadList:
+    fileUrl = getAbsoluteURL(baseUrl, download["src"])
+    if fileUrl is not None:
+        print(fileUrl)
+    urlretrieve(fileUrl, getDownloadPath(baseUrl, fileUrl, downloadDirectory))
