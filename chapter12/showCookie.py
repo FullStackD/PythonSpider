ff2d719e01a2ab7e998bb2434395e6c2c77ecf52
Monster <516094242@qq.com>
2017-10-09 20:11:49 +0800

    chapter12

---
+++ chapter12/showCookie.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+
+driver = webdriver.PhantomJS(executable_path=r"D:\phantomjs-2.1.1-windows\bin\phantomjs.exe")
+driver.get("http://pythonscraping.com")
+driver.implicitly_wait(1)
+print(driver.get_cookies())
